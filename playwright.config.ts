638b105507654aa0494b7fbd386a4cade0ef3d37
Daniel Ntege <danientege785@gmail.com>
2026-01-31 14:12:48 +0300

    feat: test

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   expect: {
     timeout: 10_000,
   },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI ? 'github' : 'list',
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
