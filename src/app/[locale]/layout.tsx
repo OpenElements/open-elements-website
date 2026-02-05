9c8563185aff50a9654a3ec19c7de60bf4da798d
Daniel Ntege <danientege785@gmail.com>
2026-02-05 19:07:11 +0300

    refactor: remove commented-out code in About and Contact pages, and clean up layout files
    Signed-off-by: Daniel Ntege <danientege785@gmail.com>

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -65,13 +65,10 @@ export default async function LocaleLayout({
 }) {
   const { locale } = await params;
 
-  // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as any)) {
     notFound();
   }
 
-  // Providing all messages to the client
-  // side is the easiest way to get started
   const messages = await getMessages();
 
   return (
