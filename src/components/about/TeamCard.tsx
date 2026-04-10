68b9b259352d27b1317e11c94e7479baca36e9ad
Daniel Ntege <danientege785@gmail.com>
2026-04-10 16:34:53 +0300

    feat: add Marc Philipp to team and update about page tests
    Signed-off-by: Daniel Ntege <danientege785@gmail.com>

--- src/components/about/TeamCard.tsx
+++ src/components/about/TeamCard.tsx
@@ -24,20 +24,22 @@ export default function TeamCard({ member }: TeamCardProps) {
           width={224}
           height={224}
         />
-        <Image
-          className="bottom-5 left-1.5 absolute rounded-full size-44 object-cover shrink-0"
-          src={member.picture}
-          alt={`${member.firstName} ${member.lastName}`}
-          width={176}
-          height={176}
-        />
-        {member.tag ? (
-          <span
-            className="pointer-events-none absolute bottom-6 left-1/2 z-10 inline-flex min-w-28 max-w-36 -translate-x-1/2 items-center justify-center rounded-full border border-white/75 px-4 py-2 text-center text-[13px] font-semibold leading-none text-white backdrop-blur-[2px]"
-            style={tagStyle}>
-            {member.tag}
-          </span>
-        ) : null}
+        <div className="bottom-5 left-1.5 absolute size-44">
+          <Image
+            className="rounded-full size-44 object-cover shrink-0"
+            src={member.picture}
+            alt={`${member.firstName} ${member.lastName}`}
+            width={176}
+            height={176}
+          />
+          {member.tag ? (
+            <span
+              className="pointer-events-none absolute top-5 left-1/2 z-10 inline-flex min-w-28 max-w-36 -translate-x-1/2 items-center justify-center rounded-full border border-white/75 px-4 py-2 text-center text-[13px] font-semibold leading-none text-white backdrop-blur-[2px]"
+              style={tagStyle}>
+              {member.tag}
+            </span>
+          ) : null}
+        </div>
       </div>
       <div className="text-center">
         <span className="font-bold text-blue text-lg transition-colors group-hover:text-purple">
