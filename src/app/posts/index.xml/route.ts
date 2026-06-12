5e1cbdc2fcb9e6e61f3354247bd2c50ba6c49487
Jessie Ssebuliba <jessiessebuliba@gmail.com>
2026-06-12 11:21:08 +0300

    add rss feed
    Signed-off-by: Jessie Ssebuliba <jessiessebuliba@gmail.com>

---
+++ src/app/posts/index.xml/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+import { buildPostsRssFeed, rssResponseHeaders } from '@/lib/rss';
+
+export const dynamic = 'force-static';
+
+export function GET() {
+  const xml = buildPostsRssFeed('/posts/index.xml');
+  return new NextResponse(xml, {
+    status: 200,
+    headers: rssResponseHeaders(),
+  });
+}
